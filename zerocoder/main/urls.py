ba75aa26a18bcca64bdc0e8151c8d232f687e228
Анатолий <lan2002@yandex.ru>
2024-06-30 02:14:31 +0300

    (update) python files

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new)
+    path('', views.index, name='home'),
+    path('page1', views.page1, name='page1'),
+    path('page2', views.page2, name='page2'),
+    path('page3', views.page3, name='page3')
 ]
