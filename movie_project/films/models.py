693d3465c64cd1826ed710ddfd3508f5e444dfb1
Анатолий <lan2002@yandex.ru>
2024-07-04 02:49:14 +0300

    (new) films app model

--- movie_project/films/models.py
+++ movie_project/films/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class MovieDatabase(models.Model):
+    title = models.CharField('Название фильма', max_length=50)
+    short_description = models.CharField('Описание фильма', max_length=200)
+    text = models.TextField('Отзыв о фильме')
+    pub_date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
